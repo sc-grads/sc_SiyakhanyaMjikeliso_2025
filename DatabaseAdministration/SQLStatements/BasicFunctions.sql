e957fe119125456c8201d06d68e4d1eea485ef05
Siyakhanya <siyakhanya.mjikeliso@sambeconsulting.com>
2025-03-12 08:49:32 +0200

    Using basic function like max, min, count, upper, lower, covert, getdate()

---
+++ DatabaseAdministration/SQLStatements/BasicFunctions.sql
@@ -0,0 +1,25 @@
+SELECT max(rate) -- find Max value 
+  FROM [AdventureWorks2022].[HumanResources].[EmployeePayHistory]
+
+  SELECT min(rate) -- Find min value
+  FROM [AdventureWorks2022].[HumanResources].[EmployeePayHistory]
+
+  SELECT count(rate)
+  FROM [AdventureWorks2022].[HumanResources].[EmployeePayHistory]
+
+  print upper('dotnet') --  Convert it into capital letters
+
+  print lower('DOTNET') --- Convert it into small latters
+
+  print convert(int,15.56) --- convert number with decimal into a interger
+
+  print getdate()   -- returns todays date
+
+  print day(getdate()) --- return today date
+
+  print month(getdate())  -- return the month
+
+  print
+
+
+
