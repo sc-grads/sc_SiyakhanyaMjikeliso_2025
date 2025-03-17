e6096402ceed2229beb79f9efa3bec2a0dedb13c
Siyakhanya <siyakhanya.mjikeliso@sambeconsulting.com>
2025-03-17 12:56:23 +0200

    Backing up database using differential database

---
+++ DatabaseAdministration/SQLStatements/DifferentialBackup.sql
@@ -0,0 +1,2 @@
+BACKUP DATABASE [AdventureWorks2022] TO  DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\Backup\AdventureWorks2022_diff_1.diff' WITH  DIFFERENTIAL , NOFORMAT, NOINIT,  NAME = N'AdventureWorks2022-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
+GO
