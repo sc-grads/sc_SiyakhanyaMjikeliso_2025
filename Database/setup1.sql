59f09bee356b2928c46618e71e09bab6f8eaa6fd
Siyakhanya2002 <siyakhanya.mjikeliso@sambeconsulting.com>
2025-04-02 22:37:32 +0200

    Update setup1.sql

--- Database/setup1.sql
+++ Database/setup1.sql
@@ -1,8 +1,8 @@
 -- 1. Create database if it doesn't exist
 BEGIN TRY
-    IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'AutoDBSiya')
+    IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'AutoDBSiya1')
     BEGIN
-        CREATE DATABASE AutoDBSiya;
+        CREATE DATABASE AutoDBSiya1;
         PRINT '✅ Database created successfully.';
     END
     ELSE
@@ -15,7 +15,7 @@ END CATCH
 GO
 
 -- 2. Switch to the new database
-USE AutoDBSiya;
+USE AutoDBSiya1;
 GO
 
 -- 3. Drop stored procedure if it exists
